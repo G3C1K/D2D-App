81393e2a8727593e7d3c915e51dcd1a34cd00d39
Patryk Przybyszewski <superkarmel123@gmail.com>
2020-05-02 16:20:12 +0200

    dodanie projektu jeszcze raz

---
+++ D2DUIvX/ImageViewActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Views;
+using Android.Widget;
+using D2DUIvX;
+
+namespace D2DUIX
+{
+    [Activity(Label = "ImageViewActivity")]
+    public class ImageViewActivity : AppCompatActivity
+    {
+
+        public CommClientAndroid client;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.imageview_submenu);
+
+            client = ClientHolder.Client;
+
+
+
+
+            // Create your application here
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.basic_submenu_toolbar, menu);
+
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowHomeEnabled(true);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item.ItemId == Android.Resource.Id.Home)
+            {
+                this.Finish();
+                return true;
+            }
+
+
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
